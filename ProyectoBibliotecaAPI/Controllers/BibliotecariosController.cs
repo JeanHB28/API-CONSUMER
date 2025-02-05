7553e2961639b142e56f539e0d423889bd3932e1
jeanh <jeanh@JEANHIRUJO>
2025-02-05 19:13:08 -0400

    Se agregaron los controladores de cada entidad

---
+++ ProyectoBibliotecaAPI/Controllers/BibliotecariosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoBibliotecaAPI.Data;
+using ProyectoBibliotecaAPI.Models;
+
+namespace ProyectoBibliotecaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BibliotecariosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BibliotecariosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Bibliotecarios
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Bibliotecario>>> GetBibliotecario()
+        {
+            return await _context.Bibliotecario.ToListAsync();
+        }
+
+        // GET: api/Bibliotecarios/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Bibliotecario>> GetBibliotecario(int id)
+        {
+            var bibliotecario = await _context.Bibliotecario.FindAsync(id);
+
+            if (bibliotecario == null)
+            {
+                return NotFound();
+            }
+
+            return bibliotecario;
+        }
+
+        // PUT: api/Bibliotecarios/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBibliotecario(int id, Bibliotecario bibliotecario)
+        {
+            if (id != bibliotecario.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bibliotecario).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BibliotecarioExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Bibliotecarios
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Bibliotecario>> PostBibliotecario(Bibliotecario bibliotecario)
+        {
+            _context.Bibliotecario.Add(bibliotecario);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBibliotecario", new { id = bibliotecario.ID }, bibliotecario);
+        }
+
+        // DELETE: api/Bibliotecarios/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBibliotecario(int id)
+        {
+            var bibliotecario = await _context.Bibliotecario.FindAsync(id);
+            if (bibliotecario == null)
+            {
+                return NotFound();
+            }
+
+            _context.Bibliotecario.Remove(bibliotecario);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BibliotecarioExists(int id)
+        {
+            return _context.Bibliotecario.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ ProyectoBibliotecaAPI/Controllers/RecomendacionsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProyectoBibliotecaAPI.Data;
+using ProyectoBibliotecaAPI.Models;
+
+namespace ProyectoBibliotecaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecomendacionsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RecomendacionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Recomendacions
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recomendacion>>> GetRecomendacion()
+        {
+            return await _context.Recomendacion.ToListAsync();
+        }
+
+        // GET: api/Recomendacions/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recomendacion>> GetRecomendacion(int id)
+        {
+            var recomendacion = await _context.Recomendacion.FindAsync(id);
+
+            if (recomendacion == null)
+            {
+                return NotFound();
+            }
+
+            return recomendacion;
+        }
+
+        // PUT: api/Recomendacions/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecomendacion(int id, Recomendacion recomendacion)
+        {
+            if (id != recomendacion.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recomendacion).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecomendacionExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Recomendacions
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Recomendacion>> PostRecomendacion(Recomendacion recomendacion)
+        {
+            _context.Recomendacion.Add(recomendacion);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRecomendacion", new { id = recomendacion.ID }, recomendacion);
+        }
+
+        // DELETE: api/Recomendacions/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecomendacion(int id)
+        {
+            var recomendacion = await _context.Recomendacion.FindAsync(id);
+            if (recomendacion == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recomendacion.Remove(recomendacion);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RecomendacionExists(int id)
+        {
+            return _context.Recomendacion.Any(e => e.ID == id);
+        }
+    }
+}
